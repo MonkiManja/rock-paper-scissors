a4761f202d55243df4dfce6ca489e6707ce878c1
MonkiManja <122291289+MonkiManja@users.noreply.github.com>
2023-02-12 19:05:27 -0300

    Game working

--- main.js
+++ main.js
@@ -25,22 +25,15 @@ function round(pSelect, cSelect){//Takes player selection and computer selection
     if(pSelect == cSelect){ 
         console.log("It's a tie!")
     } else if ((pSelect == 0 && cSelect == 2) || (pSelect == 1 && cSelect == 0) || (pSelect == 2 && cSelect == 1) ){
-        console.log("You win this round!");
+        console.log("You win this round! " + numTranslator(pSelect) + " BEATS " + numTranslator(cSelect));
         robotLives -= 1;
+        return "You win this round! " + numTranslator(pSelect) + " BEATS " + numTranslator(cSelect);
     } else {
-        console.log("You lost this round!");
+        console.log("You lost this round! " + numTranslator(cSelect) + " BEATS " + numTranslator(pSelect));
         userLives -= 1;
+        return "You lost this round! " + numTranslator(cSelect) + " BEATS " + numTranslator(pSelect);
     }
-    console.log("LIVES: -Computer: "+ robotLives + "   -You: " + userLives)
-    if(userLives == 0){
-        console.log("!!!GAME OVER!!!");
-        userLives = 5;
-        console.log("Lives restarted")
-    } else if(robotLives == 0){
-        console.log("YOU WON THE MATCH");
-        robotLives = 5;
-        console.log("Lives restarted")
-    }
+
 }
 
 
@@ -55,3 +48,19 @@ function numTranslator(num){
         return "scissors"
     }
 }
+
+function game(){ 
+    while(userLives > 0 && robotLives > 0){
+        let pInput;
+        round(parseInt(prompt(pInput)), getComputerChoice());
+
+        console.log("LIVES: -Computer: "+ robotLives + "   -You: " + userLives)
+        if(userLives == 0){
+            console.log("!!!GAME OVER!!!");
+        } else if(robotLives == 0){
+            console.log("YOU WON THE MATCH");
+        }
+    }
+}
+
+game();
